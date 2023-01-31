c7da8d78625e2d30ada6518732403bebc520d9b4
Yannick Guern <dev@guern.eu>
2023-01-31 17:26:42 +0100

    extraction de paramètres de l'URL

--- exemple.sh
+++ exemple.sh
@@ -1,6 +1,1 @@
-read -p "Connection sur le port 8080" -n1 -s
-echo
-curl -v 127.1:8080
-read -p "Connection sur le port 8081" -n1 -s
-echo
-curl -v 127.1:8081
+curl -v 127.1:8080/hello/$1/$2
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
-use actix_web::{App, HttpServer};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+// Directive de déclaration d'une route "/hello" en verbe GET
+// avec paramètres
+#[get("/hello/{name}/{number}")]
+// Routine de réponses qui accepte un paramètre name de type String
+async fn hello(data: web::Path<(String, u32)>) -> impl Responder {
+    let (name, number) = data.into_inner();
+    // Concaténation avec le paramètre name
+    let body = format!("Hello {name} {number}!\n");
+    // Création de la réponse en 200
+    // avec le body créé
+    HttpResponse::Ok().body(body)
+}
 
 // Directive de déclaration du main Actix Web
 #[actix_web::main]
 // Point d'entrée de Actix Web
 async fn main() -> std::io::Result<()> {
     // Déclaration du serveur HTTP de réponses
-    HttpServer::new(App::new)
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            // on enregistre le service "hello"
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
